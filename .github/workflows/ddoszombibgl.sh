77cce19463147feb95a787452081ddb79c4b297a
bagol422 <86297832+bagol422@users.noreply.github.com>
2021-06-27 04:43:43 +0700

    Update ddoszombibgl.sh

--- .github/workflows/ddoszombibgl.sh
+++ .github/workflows/ddoszombibgl.sh
@@ -1,41 +1,7 @@
-#! /bin/bash
-
-# Make Instance Ready for Remote Desktop or RDP
-
-b='\033[1m'
-r='\E[31m'
-g='\E[32m'
-c='\E[36m'
-endc='\E[0m'
-enda='\033[0m'
-
-clear
-
-# Branding
-
-printf """$c$b
-██████╗░██████╗░░█████╗░░██████╗  ██████╗░░█████╗░░██████╗░░█████╗░██╗░░░░░
-██╔══██╗██╔══██╗██╔══██╗██╔════╝  ██╔══██╗██╔══██╗██╔════╝░██╔══██╗██║░░░░░
-██║░░██║██║░░██║██║░░██║╚█████╗░  ██████╦╝███████║██║░░██╗░██║░░██║██║░░░░░
-██║░░██║██║░░██║██║░░██║░╚═══██╗  ██╔══██╗██╔══██║██║░░╚██╗██║░░██║██║░░░░░
-██████╔╝██████╔╝╚█████╔╝██████╔╝  ██████╦╝██║░░██║╚██████╔╝╚█████╔╝███████╗
-╚═════╝░╚═════╝░░╚════╝░╚═════╝░  ╚═════╝░╚═╝░░╚═╝░╚═════╝░░╚════╝░╚══════╝
-░█████╗░██╗░░░██╗██████╗░███████╗██████╗░
-██╔══██╗╚██╗░██╔╝██╔══██╗██╔════╝██╔══██╗
-██║░░╚═╝░╚████╔╝░██████╦╝█████╗░░██████╔╝
-██║░░██╗░░╚██╔╝░░██╔══██╗██╔══╝░░██╔══██╗
-╚█████╔╝░░░██║░░░██████╦╝███████╗██║░░██║
-░╚════╝░░░░╚═╝░░░╚═════╝░╚══════╝╚═╝░░╚═╝
-    $r BAGOL $c 
-$endc$enda""";
-
-
-# Used Two if else type statements, one is simple second is complex. So, don't get confused or fear by seeing complex if else statement '^^.
-
-! apt update && apt upgrade
-! apt install python
-! apt install python2
-! apt install git
+! apt update && apt upgrade -y
+! apt install python -y
+! apt install python2 -y
+! apt install git -y
 ! git clone https://github.com/MrTamfanX/MCIDDOS
 ! cd MCIDDOS
 ! ls 
